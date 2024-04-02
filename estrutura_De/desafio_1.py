37f865e805c28b1c2746f2dcbead74a5869a2d45
Júlia Isadora Amorim <110129192+juliaIamorim@users.noreply.github.com>
2024-04-02 14:25:20 +0000

    desafio

--- estrutura_De/desafio_1.py
+++ estrutura_De/desafio_1.py
@@ -9,7 +9,7 @@
 
 import numpy as np
 
-array = np.zeros(5) #Criar um array de 5 posições e preenchê-lo com zeros
+array = [None] * 5 #Criar um array de 5 posições e preenchê-lo com nada
 tam_array = 5 #Tamanho do array
 re = 0 #Inicializando a variável r para controle de inserção de elementos no array (r = ré)
 frente = 0 #Inicializando a variável f para controle de remoção de elementos no array (f = frente)
@@ -18,46 +18,81 @@ frente = 0 #Inicializando a variável f para controle de remoção de elementos 
 #INSERINDO NOVO ELEMENTO:
 
 # Função para primeiro verificar se a fila está vazia
-def fila_vazia(): 
-    if re == frente: #Verifica se a ré e a frente estão na mesma posição
-        print('Fila vazia')
-    elif re == tam_array:
-        print(f'Fila cheia OVERFLOW,\n{array}')
-    elif 
+def inserir(elemento): 
+    global re
+    global frente
+
+    print(f'Ré {re}')
+    print(f'Frente {frente}')
     
-    else:
-        print('Fila cheia')
-   
+
+    #Verificando se tem 2 espaços ou mais vazios para voltar a ré
+    qtd = 0
+    for i in range(tam_array):
+        if array[i] == None:
+            qtd = qtd +  1
+
+        if qtd >= 2 and re == tam_array:
+            re = 0
+    
+    if re == frente:
+        re = 0
+        frente = 0
 
 
-
-
-def inserir_elemento(elemento):
-    global tam_array
-    global re_insercao
-
-
-
+    if re == tam_array:
+            print(f'\nFila cheia = OVERFLOW\n')
+    elif re + 1 == frente:
+        print(f'Falso Overflow!')
+    elif re + 1 != frente:
+        array[re] = elemento
+        re = re + 1
+    
+    print(array) 
+    print(f'Ré {re}')
+    print(f'Frente {frente}')
 
 #Função para remover elemento
+def remover():
+    global frente
+    global re
+
+    print(re)
+    print(frente)
+    
+    if  frente - 1 == re:
+        frente = 0
+
+    if re == frente:
+        print('Fila vazia = UNDERFLOW\n')
+
+    else:
+        array[frente] = None
+        frente = frente + 1
+    print(array)
+    print(re)
+    print(frente)
+    
+
+
+
+
+
 #Função para consultar elemento
 
 
 while True:
-    opcao = 0
-    print(int(input('Menu:\n0- SAIR\n1- Inserir novo elemento\n2- Remover elemento\n3- Exibir elemento\n4- Encontrar elemento\n\nEscolha uma das opções acima: ')))
+    opcao = int(input('Menu:\n0- SAIR\n1- Inserir novo elemento\n2- Remover elemento\n3- Exibir elemento\n4- Encontrar elemento\n\nEscolha uma das opções acima: '))
 
     if opcao == 0:
         break
 
     elif opcao == 1: #Inserir novo elemento 
-        inserir_elemento()
+        elemento = int(input('Digite o elemento que quer adicionar: '))
+        inserir(elemento)
 
     elif opcao == 2: #Remover elemento
-        if len(array) == 0:
-            print('UNDERFLOW... Fila vazia\n')
-        else:
-            remover_elemento()
+            remover()
 
     elif opcao == 3: #Exibir os elementos 
         print(array)
