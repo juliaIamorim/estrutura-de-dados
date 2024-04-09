b730ff9a0a72767b06bcde76e3bc6ec546a68bb2
Júlia Isadora Amorim <110129192+juliaIamorim@users.noreply.github.com>
2024-04-09 00:54:02 +0000

    desafio pronto

--- estrutura_De/desafio_1.py
+++ estrutura_De/desafio_1.py
@@ -3,84 +3,89 @@
     # Saídas pela frente
 # Inserir um novo elemento
 # Remover um elemento
-# Exibir um elemento
 # Encontrar elemento no array
 # DESAFIO: fazer o controle do false overflow (fila circular)
 
 import numpy as np
 
-array = [None] * 5 #Criar um array de 5 posições e preenchê-lo com nada
+array = [None] * 5 #Criar um array de 5 posições e preenchê-lo com um espaço vazio
 tam_array = 5 #Tamanho do array
 re = 0 #Inicializando a variável r para controle de inserção de elementos no array (r = ré)
 frente = 0 #Inicializando a variável f para controle de remoção de elementos no array (f = frente)
 
 
-#INSERINDO NOVO ELEMENTO:
+#FUNÇÃO PARA INSERIR NOVO ELEMENTO:
 
-# Função para primeiro verificar se a fila está vazia
+# Função para primeiro verificar as condições, antes de inserir um novo elemento.
 def inserir(elemento): 
     global re
     global frente
 
-    print(f'Ré {re}')
-    print(f'Frente {frente}')
-    
+    #((f == 0 or f == 1) and r == TAMANHO) or ((r+1) == f)
 
-    #Verificando se tem 2 espaços ou mais vazios para voltar a ré
-    qtd = 0
+    #Verificando se tem 2 ou mais espaços vazios para circular, e a ré voltar a posição 0 
+    espaco = 0
     for i in range(tam_array):
         if array[i] == None:
-            qtd = qtd +  1
+            espaco = espaco +  1
 
-        if qtd >= 2 and re == tam_array:
+        if espaco >= 2 and re == tam_array:
             re = 0
-    
-    if re == frente:
-        re = 0
-        frente = 0
 
+    #Condição de fila vazia 
+    #if re == frente: 
+        #re = 0
+        #frente = 0
 
-    if re == tam_array:
+    # #Condição para sinalizar um falso overflow, não insere
+    if re + 1 == frente or (re == tam_array and espaco == 1): 
+        print(f'\nFalso Overflow!') 
+    #Condição de fila cheia
+    elif re == tam_array: 
             print(f'\nFila cheia = OVERFLOW\n')
-    elif re + 1 == frente:
-        print(f'Falso Overflow!')
+
+    #Condição para inserir o elemento
     elif re + 1 != frente:
         array[re] = elemento
         re = re + 1
     
-    print(array) 
-    print(f'Ré {re}')
-    print(f'Frente {frente}')
+    print(f'\n{array} Ré:{re} Frente:{frente}\n') 
 
-#Função para remover elemento
+
+# FUNÇÃO PARA REMOVER ELEMENTO
 def remover():
     global frente
     global re
-
-    print(re)
-    print(frente)
     
-    if  frente - 1 == re:
-        frente = 0
-
+    
     if re == frente:
-        print('Fila vazia = UNDERFLOW\n')
+        print('\nFila vazia = UNDERFLOW\n')
 
     else:
+        if frente + 1 > tam_array:
+            frente = 0
+
         array[frente] = None
         frente = frente + 1
-    print(array)
-    print(re)
-    print(frente)
+
+    if  frente - 1 == re:
+        frente = 0
+    print(f'\n{array} Ré:{re} Frente:{frente}\n') 
     
 
+# FUNÇÃO PARA ENCONTRAR UM ELEMENTO NO ARRAY:
+def encontrar(array, elemento):
+    procura = True #procura vai ser true até ser encontrada no array
+    for i in range(tam_array):
+        if array[i] == elemento:
+            procura = False
+            print(f"\nO {elemento} está no índice {i} do array!")
+        
+    if procura:
+        print("\nO elemento não está no ARRAY!\n") 
 
 
-
-
-#Função para consultar elemento
-
-
+#Menu para o usuário escolher o que ele quer verificar:
 while True:
     opcao = int(input('Menu:\n0- SAIR\n1- Inserir novo elemento\n2- Remover elemento\n3- Exibir elemento\n4- Encontrar elemento\n\nEscolha uma das opções acima: '))
 
@@ -92,7 +97,7 @@ while True:
         inserir(elemento)
 
     elif opcao == 2: #Remover elemento
-            remover()
+        remover()
 
     elif opcao == 3: #Exibir os elementos 
         print(array)
@@ -100,7 +105,7 @@ while True:
 
     elif opcao == 4: #Procurar elemento
         procura = int(input('Digite o elemento que você quer procurar no array: '))
-        consultar_elemento(procura)
+        encontrar(array, procura)
 
     else:
         print('Esta não é uma opção válida!')
